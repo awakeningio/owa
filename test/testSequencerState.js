3e11111bffaab93291c978e9d047e7142087907b
Colin Sullivan <colin@colin-sullivan.net>
2018-03-02 11:13:13 -0800

    getting test in place for validating initial state

---
+++ test/testSequencerState.js
@@ -0,0 +1,53 @@
+import { expect } from 'chai';
+
+import { OWA_READY_STATES, SESSION_PHASES } from '../src/constants'
+
+import {configureStore, configureLinkStore} from "../src/configureStore"
+import OWAController from "../src/OWAController"
+
+describe("Sequencer States", function () {
+  var store = configureStore();
+  var abletonLinkStateStore = configureLinkStore();
+  var owaController = new OWAController(store, {
+    linkStateStore: abletonLinkStateStore
+  });
+  var state = store.getState();
+
+  it("should init properly", function (done) {
+    var unsub, soundReady = null;
+
+    unsub = store.subscribe(() => {
+      state = store.getState();
+      if (state.soundReady !== soundReady) {
+        soundReady = state.soundReady;
+
+        if (soundReady === OWA_READY_STATES.READY) {
+          unsub();
+          done();
+        }
+      }
+    });
+  });
+
+  it("should have proper number of levels and segments", function () {
+
+    expect(state.levels.allIds.length).to.equal(3);
+    expect(state.segments.allIds.length).to.equal(12);
+
+  });
+
+  it("should be in IDLE phase", function () {
+    expect(state.sessionPhase).to.equal(SESSION_PHASES.IDLE);
+  });
+  
+  it("should close down cleanly", function (done) {
+    owaController.quit().then(() => {
+      done();
+      process.exit(0);
+    }).catch((err) => {
+      done(err);
+      process.exit(1);
+    });
+  });
+
+});

f5254ae707868d0f8e9d2e0fe3310c2394a86779
Colin Sullivan <colin@colin-sullivan.net>
2018-05-19 12:21:39 -0700

    an idle model animation between levels

--- src/IdleModeAnimation.js
+++ src/IdleModeAnimation.js
@@ -33,7 +33,8 @@ class IdleModeAnimation extends ControllerWithStore {
       brightness: 1
     };
 
-    this.tween = new TWEEN.Tween(initial)
+    // all at once
+    this.allFadeTween = new TWEEN.Tween(initial)
       .to(dest, 4000)
       .easing(TWEEN.Easing.Sinusoidal.In)
       .yoyo(true)
@@ -46,13 +47,59 @@ class IdleModeAnimation extends ControllerWithStore {
         }
       });
 
+    this.level6FadeTween = new TWEEN.Tween(Object.assign({}, initial))
+      .to(Object.assign({}, dest), 4000)
+      .easing(TWEEN.Easing.Sinusoidal.In)
+      .yoyo(true)
+      .repeat(Infinity)
+      .onUpdate((props) => {
+        var i;
+        // turn all pixels to black
+        for (i = 0; i < this.params.levelPixels.level_6.length; i++) {
+          this.params.levelPixels.level_6.setPixel(i, props.brightness * 255, props.brightness * 255, props.brightness * 255);
+        }
+      });
+
+    this.level4FadeTween = new TWEEN.Tween(Object.assign({}, initial))
+      .to(Object.assign({}, dest), 4000)
+      .delay(500)
+      .easing(TWEEN.Easing.Sinusoidal.In)
+      .yoyo(true)
+      .repeat(Infinity)
+      .onUpdate((props) => {
+        var i;
+        // turn all pixels to black
+        for (i = 0; i < this.params.levelPixels.level_4.length; i++) {
+          this.params.levelPixels.level_4.setPixel(i, props.brightness * 255, props.brightness * 255, props.brightness * 255);
+        }
+      });
+
+    this.level2FadeTween = new TWEEN.Tween(Object.assign({}, initial))
+      .to(Object.assign({}, dest), 4000)
+      .delay(1000)
+      .easing(TWEEN.Easing.Sinusoidal.In)
+      .yoyo(true)
+      .repeat(Infinity)
+      .onUpdate((props) => {
+        var i;
+        // turn all pixels to black
+        for (i = 0; i < this.params.levelPixels.level_2.length; i++) {
+          this.params.levelPixels.level_2.setPixel(i, props.brightness * 255, props.brightness * 255, props.brightness * 255);
+        }
+      });
 
   }
   start () {
-    this.tween.start();
+    //this.allFadeTween.start();
+    this.level6FadeTween.start();
+    this.level4FadeTween.start();
+    this.level2FadeTween.start();
   }
   stop () {
-    this.tween.stop();
+    this.allFadeTween.stop();
+    this.level6FadeTween.stop();
+    this.level4FadeTween.stop();
+    this.level2FadeTween.stop();
     this.build();
   }
 }

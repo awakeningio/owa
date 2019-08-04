9b293809b004a9f2326e8bb8782208972e714ba3
Colin Sullivan <colin@colin-sullivan.net>
2019-08-04 11:53:06 -0700

    Fixed test

--- test/testSequentialSequencer.js
+++ test/testSequentialSequencer.js
@@ -1,41 +1,40 @@
-import { expect } from 'chai';
+import { expect } from "chai";
 
-import awakeningSequencers from 'awakening-sequencers';
-import { OWA_READY_STATES, SESSION_PHASES } from 'owa/constants'
-import configureStore from "../src/configureStore"
-import OWAController from "../src/OWAController"
-import { create_segmentId } from 'owa/models'
-import { createInitialState } from 'owa/state';
+import awakeningSequencers from "awakening-sequencers";
+import { OWA_READY_STATES, SESSION_PHASES } from "owa/constants";
+import configureStore from "../src/configureStore";
+import OWAController from "../src/OWAController";
+import { create_segmentId } from "owa/models";
+import { createInitialState } from "owa/state";
 import {
   getSegmentIdToBufName,
   getLevel6Sequencers,
   getSegmentIdToSequencerId
-} from '../src/selectors';
-import * as actions from '../src/actions'
+} from "../src/selectors";
+import * as actions from "../src/actions";
 
 const PLAYING_STATES = awakeningSequencers.PLAYING_STATES;
 
-describe("Sequential Sequencer", function () {
+describe("Sequential Sequencer", function() {
   var store,
     state,
     segment,
     sequencer,
     level,
     secondSegment,
-    segmentIdToBufName,
-    owaController;
+    segmentIdToBufName;
 
-  it("should init properly", function (done) {
+  it("should init properly", function(done) {
     var unsub, soundReady;
-    const initialState = createInitialState()
-    
+    const initialState = createInitialState();
+
     initialState.sessionPhaseDurations[SESSION_PHASES.QUEUE_TRANS_4] = 2;
     initialState.sessionPhaseDurations[SESSION_PHASES.TRANS_4] = 2;
     initialState.sessionPhase = SESSION_PHASES.PLAYING_6;
-    getLevel6Sequencers(initialState).forEach(function (seq) {
+    getLevel6Sequencers(initialState).forEach(function(seq) {
       seq.playingState = PLAYING_STATES.PLAYING;
     });
-    
+
     store = configureStore(initialState);
     state = store.getState();
     soundReady = state.soundReady;
@@ -55,35 +54,33 @@ describe("Sequential Sequencer", function () {
       disableInactivity: true
     });
   });
-  
+
   // start testing level 4
-  it("should immediately start transition when level4 button is pressed", function () {
-    level = state.levels.byId['level_4'];
+  it("should immediately start transition when level4 button is pressed", function() {
+    level = state.levels.byId["level_4"];
     store.dispatch(actions.buttonPressed(level.levelId, 0));
     state = store.getState();
     segmentIdToBufName = getSegmentIdToBufName(state);
     segment = state.segments.byId[create_segmentId(level.levelId, 0)];
-    sequencer = state.sequencers[
-      getSegmentIdToSequencerId(state)[segment.segmentId]
-    ];
+    sequencer =
+      state.sequencers[getSegmentIdToSequencerId(state)[segment.segmentId]];
     expect(state.sessionPhase).to.equal(SESSION_PHASES.QUEUE_TRANS_4);
   });
-  
-  it("sequencer should have updated playback order on button press", function () {
-    level = state.levels.byId['level_4'];
+
+  it("should have updated playback order on button press", function() {
+    level = state.levels.byId["level_4"];
     segment = state.segments.byId[create_segmentId(level.levelId, 0)];
-    sequencer = state.sequencers[
-      getSegmentIdToSequencerId(state)[segment.segmentId]
-    ];
-    expect(sequencer).to.have.property('bufSequence');
-    expect(sequencer.bufSequence).to.be.a('array');
+    sequencer =
+      state.sequencers[getSegmentIdToSequencerId(state)[segment.segmentId]];
+    expect(sequencer).to.have.property("bufSequence");
+    expect(sequencer.bufSequence).to.be.a("array");
     expect(sequencer.bufSequence).to.have.lengthOf(1);
     expect(sequencer.bufSequence[0]).to.equal(
       segmentIdToBufName[segment.segmentId]
     );
   });
 
-  it('should transition to PLAYING_4', function (done) {
+  it("should transition to PLAYING_4", function(done) {
     let sessionPhase = store.getState().sessionPhase;
     const sessionPhaseSequence = [
       SESSION_PHASES.TRANS_4,
@@ -103,30 +100,34 @@ describe("Sequential Sequencer", function () {
       }
     });
   });
-  
-  it("should not transition sessionPhase on button press", function () {
+
+  it("should not transition sessionPhase on button press", function() {
     secondSegment = state.segments.byId[create_segmentId(level.levelId, 1)];
     store.dispatch(actions.buttonPressed(level.levelId, 1));
     state = store.getState();
     segmentIdToBufName = getSegmentIdToBufName(state);
-    level = state.levels.byId['level_4'];
-    sequencer = state.sequencers[
-      getSegmentIdToSequencerId(state)[segment.segmentId]
-    ];
+    level = state.levels.byId["level_4"];
+    sequencer =
+      state.sequencers[getSegmentIdToSequencerId(state)[segment.segmentId]];
     expect(state.sessionPhase).to.equal(SESSION_PHASES.PLAYING_4);
   });
 
-  it("should have updated playback order because button press", function () {
+  it("should have updated playback order on 2nd button press", function() {
     expect(sequencer.bufSequence).to.have.lengthOf(2);
-    expect(sequencer.bufSequence[0]).to.equal(
-      segmentIdToBufName[secondSegment.segmentId]
-    );
     expect(sequencer.bufSequence[1]).to.equal(
-      segmentIdToBufName[segment.segmentId]
+      segmentIdToBufName[secondSegment.segmentId],
+      "new bufSequence should have been inserted into second slot"
+    );
+    expect(sequencer.bufSequence[0]).to.equal(
+      segmentIdToBufName[segment.segmentId],
+      "first slot of bufSequence should have remained"
     );
   });
 
-  it("should close down cleanly", function (done) {
-    this.owaController.quit().then(() => done()).catch(done);
+  it("should close down cleanly", function(done) {
+    this.owaController
+      .quit()
+      .then(() => done())
+      .catch(done);
   });
 });

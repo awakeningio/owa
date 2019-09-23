99e8366cf3af851d015c3796ba6f8c5dd661a5cb
Colin Sullivan <colin@colin-sullivan.net>
2019-09-22 22:39:37 -0700

    retrieve sounds url

--- retrieve_sounds.sh
+++ retrieve_sounds.sh
@@ -2,7 +2,5 @@
 
 COMMIT=`git show --oneline|head -1|awk '{print $1}'`
 SOUNDS_ARCHIVE_NAME=sounds_${COMMIT}.zip
-#wget https://owasounds.awakening.io/by_commit/$SOUNDS_ARCHIVE_NAME
-#https://s3-us-west-1.amazonaws.com/owasounds.awakening.io/by_commit/sounds_a4e1e00.zip
-wget https://s3-us-west-1.amazonaws.com/owasounds.awakening.io/by_commit/$SOUNDS_ARCHIVE_NAME
+wget http://owasounds.awakening.io.s3.amazonaws.com/by_commit/$SOUNDS_ARCHIVE_NAME
 unzip $SOUNDS_ARCHIVE_NAME
